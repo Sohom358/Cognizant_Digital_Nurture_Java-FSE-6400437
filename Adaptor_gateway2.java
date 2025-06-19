32080af86eb6f93bd99c82aba1358849d66025c4
Sohom358 <151345383+Sohom358@users.noreply.github.com>
2025-06-19 12:07:38 +0530

    Delete Adaptor_gateway2.java

--- Adaptor_gateway2.java
+++
@@ -1,7 +0,0 @@
-public class Adaptor_gateway2 implements PaymentProcessor{
-    public void processPayment()
-    {
-        Payment_gateway2 g2=new Payment_gateway2();
-        g2.payment2();
-    }
-}
