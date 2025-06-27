9d1ec3323d4120ad4b23d6d54b712f180c42a96f
Sohom358 <151345383+Sohom358@users.noreply.github.com>
2025-06-27 17:45:56 +0530

    Add files via upload

---
+++ Week 2_Unit Testing/SLF4J/Code/Login.java
@@ -0,0 +1,10 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Login {
+    private static final Logger l = LoggerFactory.getLogger(Login.class);
+    public static void main(String[] args) {
+    	l.error("Error occured...");
+    	l.warn("Warning...");
+    }
+}
